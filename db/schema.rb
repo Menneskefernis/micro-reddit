c6221be93bd97963130341d8136bb3c87eaa4f12
Henrik Nørgaard <menneskefernis@gmail.com>
2020-01-29 23:09:49 +0100

    Add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_215303) do
+ActiveRecord::Schema.define(version: 2020_01_29_220912) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
